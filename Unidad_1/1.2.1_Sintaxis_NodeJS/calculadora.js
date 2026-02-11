f6f006b557290f692a6edbd81455a8fb03e22925
LIX1313 <94031525+LIX1313@users.noreply.github.com>
2026-02-10 20:11:21 -0500

    Se añaden los documentos restantes
    "Completado AA 1.2.1 Sintaxis NodeJS"

---
+++ Unidad_1/1.2.1_Sintaxis_NodeJS/calculadora.js
@@ -0,0 +1,8 @@
+//Ejercicio i
+//Luis Mario Ireta Xiu
+import suma, { resta, multiplicar, exponencial } from "./operaciones.js";
+
+console.log(suma(8, 9));
+console.log(resta(5, 10));
+console.log(multiplicar(8, 9));
+console.log(exponencial(3, 3));
