40c1445152f930e25fd8ceca302c216607eafc53
LIX1313 <94031525+LIX1313@users.noreply.github.com>
2026-02-10 21:12:21 -0500

    Add labels to calculator operations output

--- Unidad_1/1.2.1_Sintaxis_NodeJS/calculadora.js
+++ Unidad_1/1.2.1_Sintaxis_NodeJS/calculadora.js
@@ -2,7 +2,7 @@
 //Luis Mario Ireta Xiu
 import suma, { resta, multiplicar, exponencial } from "./operaciones.js";
 
-console.log(suma(8, 9));
-console.log(resta(5, 10));
-console.log(multiplicar(8, 9));
-console.log(exponencial(3, 3));
+console.log("Suma: " + suma(8, 9));
+console.log("Resta: " + resta(5, 10));
+console.log("Multiplicación: " + multiplicar(8, 9));
+console.log("Exponente: " + exponencial(3, 3));
