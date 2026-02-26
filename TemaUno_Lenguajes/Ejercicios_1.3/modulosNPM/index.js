a71a061eca1e5d1c7f51f35462732c90fd27b7fa
LIX1313 <94031525+LIX1313@users.noreply.github.com>
2026-02-25 21:50:41 -0500

    Remove commented-out code and clean imports

--- TemaUno_Lenguajes/Ejercicios_1.3/modulosNPM/index.js
+++ TemaUno_Lenguajes/Ejercicios_1.3/modulosNPM/index.js
@@ -1,10 +1,8 @@
 //By: Luis Mario Ireta Xiu
 
 import generateName from "sillyname";
-import {randomSuperhero} from 'superheroes';
-var generateName = require('sillyname');
+import { randomSuperhero } from "superheroes";
+var generateName = require("sillyname");
 var sillyName = generateName();
-
 console.log(sillyName);
 console.log(randomSuperhero());
-
