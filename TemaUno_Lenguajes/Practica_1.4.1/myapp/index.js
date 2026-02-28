28f978b1dbe0bc33c60a71711de7cc608805e71f
LIX1313 <marioluis1313@hotmail.com>
2026-02-27 23:05:38 -0500

    Subiendo Practica 1.4.1

---
+++ TemaUno_Lenguajes/Practica_1.4.1/myapp/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log("Servidor corriendo en http://localhost:3000");
+});
+
+app.get("/", (req, res) => {
+    res.send("<h1>Bienvenido a mi página web</h1>");
+});
+
+app.get("/about", (req, res) => {
+    res.send("<h1>Acerca de </h1>");
+});
+
+app.use((req, res) => {
+    res.status(404).send("<h1>404 Not Found</h1>");
+});
