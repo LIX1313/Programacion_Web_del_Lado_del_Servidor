a30b380b823ec7ccf0529aa6b1aad698c48233d1
LIX1313 <94031525+LIX1313@users.noreply.github.com>
2026-03-03 03:27:52 -0500

    Add files via upload

---
+++ Ejercicios_1.4/Practica_1.4.2/peticionHTTP/index.js
@@ -0,0 +1,31 @@
+//AA 1.4.2 
+//Por Luis Mario Ireta Xiu
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("<h1> Hola Mundo </h1>");
+    res.sendStatus(200);
+});
+
+app.get('/registro', (req,res) =>{
+    res.sendStatus(201);
+});
+
+app.get('/usuario/actualizar', (req,res) => {
+    res.sendStatus(200);
+});
+
+app.get('/usuario/modificar', (req,res) => {
+    res.sendStatus(200);
+});
+
+app.get('/usuario/eliminar', (req,res) => {
+    res.sendStatus(200);
+});
+
+app.listen(port, ()=>{
+    console.log("Serverejecutandose en puerto 3000");
+});
