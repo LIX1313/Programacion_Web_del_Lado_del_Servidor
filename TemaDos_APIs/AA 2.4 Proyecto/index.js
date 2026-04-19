c1e765a72d55e3562f1a504177e73545559b8c8f
LIX1313 <marioluis1313@hotmail.com>
2026-04-19 17:28:50 -0500

    Entrega de actividad "AA 2.4 API Proyecto".

---
+++ TemaDos_APIs/AA 2.4 Proyecto/index.js
@@ -0,0 +1,47 @@
+//Por Luis Mario Ireta Xiu
+// Importación de librerías
+import axios from "axios";
+import express from "express";
+
+// Inicialización del servidor
+const app = express();
+const port = 3000;
+
+// Configuración de vistas
+app.set("views", "./views"); // Define la carpeta donde están las vistas (.ejs)
+app.set("view engine", "ejs"); // Indica a Express que usará EJS como motor de plantillas
+
+// Archivos estáticos
+app.use(express.static("public")); // Permite acceder a archivos estáticos desde la carpeta public
+
+// Ruta principal
+app.get("/", async (req, res) => {
+  try {
+    // Llamada a la API que devuelve una cita de anime
+    const result = await axios.get(
+      "https://yurippe.vercel.app/api/quotes?random=5",
+    );
+    // Extracción de datos de la respuesta
+    const quotes = result.data; // Todo el array
+
+    // Renderizado de la vista index.ejs con datos dinámicos
+    res.render("index", {
+      quotes: quotes // Se envía la cita a la vista
+    });
+
+    console.log(result.data); // Muestra la respuesta completa en consola
+  } catch (error) {
+    // Manejo de errores
+    if (error.response) {
+      console.log(error.response.data); // Error proveniente de la API
+    } else {
+      console.log("Error:", error.message); // Error generals
+    }
+    res.status(500).send("Error al obtener la cita"); // Respuesta al cliente
+  }
+});
+
+// Inicio del servidor
+app.listen(port, () => {
+  console.log("Server ejecutándose en el puerto", port);
+});
