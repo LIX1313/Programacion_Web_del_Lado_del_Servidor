b7777ad582b783ff48d89eaf3c4404b91a0fff85
LIX1313 <marioluis1313@hotmail.com>
2026-05-03 03:31:36 -0500

    Entrega de actividad "AA 3.2 Proyecto CRUD API".

--- TemaTres_Manejo_de_BBDD_y_ORM/3.2 API CRUD/index.js
+++ TemaTres_Manejo_de_BBDD_y_ORM/3.2 API CRUD/index.js
@@ -31,6 +31,27 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+app.get('/usuarios', async (req, res) => {
+    try{
+        const usuarios = await Usuario.find();
+        res.status(200).json(usuarios);
+    } catch(error){
+        console.error("Error al obtener los usuarios", error);
+        res.status(500).json({error: 'Errir al obtener los usuarios'});
+    }
+});
+
+app.get('/usuario/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+        const usuario = await Usuario.findById(id);
+        res.status(200).json(usuario);
+    } catch(error){
+        console.error("Error al obtener el usuario", error);
+        res.status(500).json({error: 'Errir al obtener el usuario'});
+    }
+});
+
 app.post('/usuarios', async (req, res) => {
     try{
         const usuario = await Usuario.create(req.body);
@@ -38,6 +59,36 @@ app.post('/usuarios', async (req, res) => {
     } catch (error){
         console.error("Error al crear el usuario", error);
         res.status(500).json({ error: 'Error al crear el usuario'});
+    }
+});
+
+app.put('/usuario/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+        const usuario = await Usuario.findByIdAndUpdate(id, req.body);
+        if(!usuario){
+            return res.status(404).json({error: 'Usuario no encontrado'});
+        }
+        const usuarioActualizado = await Usuario.findById(id);
+        res.status(200).json(usuario);
+        console.log(usuarioActualizado);
+    } catch(error){
+        console.error("Error al obtener el usuario", error);
+        res.status(500).json({error: 'Error al actualizar el usuario'});
+    }
+});
+
+app.delete('/usuario/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+        const usuario = await Usuario.findByIdAndDelete(id);
+        if(!usuario){
+            return res.status(404).json({error: 'Usuario no encontrado'});
+        }
+        res.status(200).json({message: 'Usuario eliminado'});
+    } catch(error){
+        console.error("Error al obtener el usuario", error);
+        res.status(500).json({error: 'Error al eliminar el usuario'});
     }
 });
 
